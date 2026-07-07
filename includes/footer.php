8d290b8c616f3099620839062e3c44f8e5226e40
teshan200 <git.teshan@gmail.com>
2026-07-07 22:04:04 +0530

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer class="footer">
-    <p>&copy; <?= date('Y') ?> My Blog &mdash; Web Programming Assignment</p>
+    <p>&copy; <?= date('Y') ?> My Blog &mdash; By Teshan Pamodya</p>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/easymde/dist/easymde.min.js"></script>
 <script src="<?= $root ?>assets/js/main.js"></script>
